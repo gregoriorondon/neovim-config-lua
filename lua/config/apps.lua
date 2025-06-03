55b9a996db9039f200acf90d70b79e68838824bb
gregoriorondon <gregorioalexanderondon@gmail.com>
2025-06-03 11:03:51 -0400

    Integracion de Formateador
    Se instaló "conform.nvim" junto con "manson.vim" para usar complementos de formateado de código, que en este caso se agregaron para php, blade, lua, js, html y css pero se pueden agregar mas dependiendo de los que haya disponible en "manson"

--- lua/config/apps.lua
+++ lua/config/apps.lua
@@ -1,87 +1,98 @@
 -- Cargar Lazy.nvim y configurar plugins
 require("lazy").setup({
-  -- ==========================
-  -- =========Temas============
-  -- ==========================
-  { "ayu-theme/ayu-vim", },
-  { "folke/tokyonight.nvim", lazy = false, },
-  { "ellisonleao/gruvbox.nvim", },
-  { "rakr/vim-one", priority = 1000,
-   config = function()
-       vim.cmd("colorscheme one")
-    end
-    -- opts = {},
-  },
-  { "nvim-tree/nvim-web-devicons", opts = {} },
-  {'jwalton512/vim-blade'},
+	-- ==========================
+	-- =========Temas============
+	-- ==========================
+	{ "ayu-theme/ayu-vim" },
+	{ "folke/tokyonight.nvim", lazy = false },
+	{ "ellisonleao/gruvbox.nvim" },
+	{
+		"rakr/vim-one",
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme one")
+		end,
+		-- opts = {},
+	},
+	{ "nvim-tree/nvim-web-devicons", opts = {} },
+	{ "jwalton512/vim-blade" },
 
-  -- ==========================
-  -- ======AUTOCOMPLETADO======
-  -- ==========================
-  {
-    "neoclide/coc.nvim",
-    branch = "master",
-    build = "npm ci",
-    config = function()
-      vim.cmd([[
+	-- ==========================
+	-- ======AUTOCOMPLETADO======
+	-- ==========================
+	{
+		"neoclide/coc.nvim",
+		branch = "master",
+		build = "npm ci",
+		config = function()
+			vim.cmd([[
         inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>"
       ]])
-    end
-  },
-  -- ==========================
-  -- ======PROGRAMACIÓN========
-  -- ==========================
-  {
-    'numToStr/Comment.nvim',
-  },
-  -- ========COLORES CSS=======
-  {
-    "catgoose/nvim-colorizer.lua",
-    event = "BufReadPre",
-    opts = { -- set to setup table
-    },
-  },
-  -- ==========================
-  -- =======INTERFAZ UI========
-  -- ==========================
-  {
-    "akinsho/bufferline.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-      require("bufferline").setup()
-    end,
-  },
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-  },
-  {
-	"xiyaowong/transparent.nvim",
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    lazy = false, -- neo-tree will lazily load itself
-    ---@module "neo-tree"
-    ---@type neotree.Config?
-    opts = {
-        -- fill any relevant options here
-    },
-  },    
-  
-
-
-
-
-
-
-
+		end,
+	},
+	-- ==========================
+	-- ======PROGRAMACIÓN========
+	-- ==========================
+	{
+		"numToStr/Comment.nvim",
+	},
+	-- ======FORMATEADOR=========
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"stevearc/conform.nvim",
+		dependencies = { "mason.nvim" },
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				php = { "pretty-php" },
+				blade = { "blade-formatter" },
+				javascript = { "prettierd" },
+				css = { "prettierd" },
+				html = { "prettierd" },
+			},
+		},
+	},
+	-- ========COLORES CSS=======
+	{
+		"catgoose/nvim-colorizer.lua",
+		event = "BufReadPre",
+		opts = { -- set to setup table
+		},
+	},
+	-- ==========================
+	-- =======INTERFAZ UI========
+	-- ==========================
+	{
+		"akinsho/bufferline.nvim",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		config = function()
+			require("bufferline").setup()
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		"xiyaowong/transparent.nvim",
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+		lazy = false, -- neo-tree will lazily load itself
+		---@module "neo-tree"
+		---@type neotree.Config?
+		opts = {
+			-- fill any relevant options here
+		},
+	},
 })
-
-
