54b701d3057c5b29ef1818fcc87714029bd51f98
gregoriorondon <gregorioalexanderondon@gmail.com>
2025-06-03 13:25:18 -0400

    Markdown Preview

--- lua/config/apps.lua
+++ lua/config/apps.lua
@@ -63,6 +63,19 @@ require("lazy").setup({
 		},
 	},
 	-- ==========================
+	-- =========MARKDOWN=========
+	-- ==========================
+
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && npm install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
+	-- ==========================
 	-- =======INTERFAZ UI========
 	-- ==========================
 	{
